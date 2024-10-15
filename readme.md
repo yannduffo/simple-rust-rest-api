9e1d11f35997ae042fa74ff0bfb679e87712d072
Yann Duffo <yannduffo@MacBook-Pro-de-Yann.local>
2024-10-15 13:38:01 +0200

    api ready for INFT2508 project

--- readme.md
+++ readme.md
@@ -1,17 +1,86 @@
-# Simple Rust REST API 
+# Simple Rust REST API
 
-Simple Rust REST API. Only 2 endpoints for now : GET all books and POST a new one. Use of a JSON file as a Database (in this example a book database).
+A simple REST API built with Rust. It uses **Actix Web** to create an HTTP server and use a JSON file as a database. The API allows retrieving information about restaurants and songs. It was developed as part of a cross-platform development project for the INFT2508 course at NTNU.
 
-Build with Artix-web.
+## Technologies Used
 
-## Examples : 
+- **Rust**
+- **Actix Web** - Web framework for building the REST API.
+- **Serde** - For serializing and deserializing JSON data.
+- **JSON** - Used as the database to store information about restaurants and songs.
 
-Request :
+## Data Structure
+
+The JSON file stores two sets of data:
+- **Restaurants**: Each restaurant has a name, an address, and a menu.
+- **Songs**: Each song has a name and an associated artist.
+
+### Example of the JSON structure:
+
+```json
+{
+    "restaurants": [
+        {
+            "name": "ChineseRestaurant",
+            "address": "Prinsengata 1, 7010 Trondheim",
+            "menu": [
+                {"dish": "Pizza", "price": 50},
+                {"dish": "Burger", "price": 25}
+            ]
+        },
+        {
+            "name": "MexicanRestaurant",
+            "address": "Kongensgata 10, 7020 Trondheim",
+            "menu": [
+                {"dish": "French Fries", "price": 250},
+                {"dish": "Onion Rings", "price": 150}
+            ]
+        }
+    ],
+    "songs": [
+        {
+            "name": "song1",
+            "artist": "Nils"
+        },
+        {
+            "name": "song2",
+            "artist": "Jean"
+        }
+    ]
+}
 ```
-$ curl http://127.0.0.1:8080/books
+
+## Endpoints
+
+The API endpoints are : 
+- ```GET /restaurants``` : Retrieve all restaurants.
+- ```GET /restaurants/{name}/menu``` : Retrieve the menu of a specific restaurant.
+- ```GET /songs``` : Retrieve all songs.
+- ```GET /songs/{name}``` : Retrieve information about a specific song
+
+### Example :
+Request : 
 ```
-Response form API :
+curl http://127.0.0.1:8080/songs/song1
 ```
-[{"id":1,"title":"book1","author":"Steve"},{"id":2,"title":"book2","author":"Carol"}]
+Response : 
+```json
+{
+    "name": "song1",
+    "artist": "Nils"
+}
 ```
 
+## Running the app 
+
+1. Ensure Rust and Cargo are installed on your machine.
+2. Clone the project 
+3. Run the following command to start the server: ```cargo run```
+
+The local dev server will be available at ```http://127.0.0.1:8080```.
+
+
+<br>
+<br>
+<br>
+Created by Yann Duffo
--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,93 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::sync::Mutex;
 
+//------------------------------------------- data structure declaration --------------------------------------------------------
 #[derive(Serialize, Deserialize)]
-struct Book {
-    id: u32,
-    title: String,
-    author: String,
+struct Song{
+    name:String,
+    artist:String
+}
+
+#[derive(Serialize, Deserialize)]
+struct MenuItem{
+    dish:String,
+    price:i32
+}
+
+#[derive(Serialize,Deserialize)]
+struct Restaurant{
+    name:String,
+    address:String,
+    menu: Vec<MenuItem>
+}
+
+//datafile
+#[derive(Serialize, Deserialize)]
+struct DataFile{
+    restaurants:Vec<Restaurant>,
+    songs:Vec<Song>,
 }
 
 struct AppState {
     data_file: Mutex<String>,  //JSON filepath put on a mutex to limit concurents acceses
 }
 
+//-------------------------------------------------- read/write data functions --------------------------------------------------
+
 //read data from a JSON file function
-fn read_data(file_path: &str) -> Vec<Book> {
+fn read_data(file_path: &str) -> DataFile {
     let data = fs::read_to_string(file_path).expect("Error, can't read file.");
     serde_json::from_str(&data).expect("Error in the JSON conversion")
 }
 
 //write data on a JSON file function
-fn write_data(file_path: &str, books: &Vec<Book>) {
-    let data = serde_json::to_string_pretty(&books).expect("Error while serializing the data");
+fn write_data(file_path: &str, data_file:&DataFile) {
+    let data = serde_json::to_string_pretty(&data_file).expect("Error while serializing the data");
     fs::write(file_path, data).expect("Error can't write in file");
 }
 
-#[get("/books")]
-async fn get_books(data: web::Data<AppState>) -> impl Responder {
-    let books = read_data(&data.data_file.lock().unwrap());
-    HttpResponse::Ok().json(books)
+//-------------------------------------------------- endpoints ------------------------------------------------------------------
+
+#[get("/restaurants")]
+async fn get_restaurants(data: web::Data<AppState>) -> impl Responder {
+    let data_file = read_data(&data.data_file.lock().unwrap());
+    HttpResponse::Ok().json(data_file.restaurants)
 }
 
-#[post("/books")]
-async fn add_book(data: web::Data<AppState>, new_book: web::Json<Book>) -> impl Responder {
-    let mut books = read_data(&data.data_file.lock().unwrap());
+#[get("/restaurants/{name}/menu")]
+async fn get_restaurant_menu(data: web::Data<AppState>, name: web::Path<String>) -> impl Responder {
+    let data_file = read_data(&data.data_file.lock().unwrap());
 
-    //add a new book and save data in the JSON file
-    books.push(new_book.into_inner());
-    write_data(&data.data_file.lock().unwrap(), &books);
-    
-    HttpResponse::Ok().json("Book added")
+    if let Some(restaurant) = data_file.restaurants.iter().find(|r| r.name == *name) {
+        HttpResponse::Ok().json(&restaurant.menu)
+    } else {
+        HttpResponse::NotFound().body("Restaurant not found")
+    }
 }
+
+#[get("/songs/{name}")]
+async fn get_song_info(data: web::Data<AppState>, name: web::Path<String>) -> impl Responder {
+    let data_file = read_data(&data.data_file.lock().unwrap());
+
+    if let Some(song) = data_file.songs.iter().find(|r| r.name == *name) {
+        HttpResponse::Ok().json(&song)
+    } else {
+        HttpResponse::NotFound().body("Song not found")
+    }
+}
+
+#[get("/songs")]
+async fn get_songs(data: web::Data<AppState>) -> impl Responder {
+    let data_file = read_data(&data.data_file.lock().unwrap());
+    HttpResponse::Ok().json(data_file.songs)
+}
+
+//-------------------------------------------------------- main -----------------------------------------------------------------
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //filepath of the JSON "database"
-    let data_file = "data/books.json".to_string();
+    let data_file = "data/data.json".to_string();
 
     //HTTP server strating
     HttpServer::new(move || {
@@ -54,8 +97,10 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(AppState {
                 data_file: Mutex::new(data_file.clone()),
             }))
-            .service(get_books)
-            .service(add_book)
+            .service(get_restaurants)
+            .service(get_restaurant_menu)
+            .service(get_songs)
+            .service(get_song_info)
     })
     .bind("127.0.0.1:8080")?
     .run()
