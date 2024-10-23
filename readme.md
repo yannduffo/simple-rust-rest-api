a6a5d1acb06703eb81078f5270e7241f8c6889ce
Yann Duffo <yannduffo@MacBook-Pro-de-Yann.local>
2024-10-23 16:21:58 +0200

    update for assignement6

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+Last update 23/10/2024 - Assignment6
+
 # Simple Rust REST API
 
 A simple REST API built with Rust. It uses **Actix Web** to create an HTTP server and use a JSON file as a database. The API allows retrieving information about restaurants and songs. It was developed as part of a cross-platform development project for the INFT2508 course at NTNU.
@@ -12,39 +14,45 @@ A simple REST API built with Rust. It uses **Actix Web** to create an HTTP serve
 ## Data Structure
 
 The JSON file stores two sets of data:
-- **Restaurants**: Each restaurant has a name, an address, and a menu.
-- **Songs**: Each song has a name and an associated artist.
+- **Dishes**: Each dish has a title, description, allergens, an image link, category, and price.
+- **Bookings**: Each booking has a name, phone prefix, phone number, date, and time.
 
 ### Example of the JSON structure:
 
 ```json
 {
-    "restaurants": [
+    "dishes": [
         {
-            "name": "ChineseRestaurant",
-            "address": "Prinsengata 1, 7010 Trondheim",
-            "menu": [
-                {"dish": "Pizza", "price": 50},
-                {"dish": "Burger", "price": 25}
-            ]
+            "title": "Margherita Pizza",
+            "description": "Tomato, Mozzarella, Basil, Olive Oil",
+            "allergens": "Cheese, Olive oil",
+            "image": "dishImages.pizza_margherita",
+            "category": "pizza",
+            "price": 10
         },
         {
-            "name": "MexicanRestaurant",
-            "address": "Kongensgata 10, 7020 Trondheim",
-            "menu": [
-                {"dish": "French Fries", "price": 250},
-                {"dish": "Onion Rings", "price": 150}
-            ]
+            "title": "Four Cheese Pizza",
+            "description": "Tomato, Mozzarella, Gorgonzola, Parmesan, Fontina, Provolone",
+            "allergens": "Cheese",
+            "image": "dishImages.pizza_four_cheese",
+            "category": "pizza",
+            "price": 13
         }
     ],
-    "songs": [
+    "bookings": [
         {
-            "name": "song1",
-            "artist": "Nils"
+            "name": "Olivier Michel",
+            "phonePrefix": "+33",
+            "phoneNumber": "123456789",
+            "date": "12/12/2024",
+            "time": "18h"
         },
         {
-            "name": "song2",
-            "artist": "Jean"
+            "name": "Jean Mathieu",
+            "phonePrefix": "+33",
+            "phoneNumber": "123456789",
+            "date": "13/12/2024",
+            "time": "20h30"
         }
     ]
 }
@@ -53,22 +61,53 @@ The JSON file stores two sets of data:
 ## Endpoints
 
 The API endpoints are : 
-- ```GET /restaurants``` : Retrieve all restaurants.
-- ```GET /restaurants/{name}/menu``` : Retrieve the menu of a specific restaurant.
-- ```GET /songs``` : Retrieve all songs.
-- ```GET /songs/{name}``` : Retrieve information about a specific song
+- ```GET /dishes``` : Retrieve all available dishes.
+- ```POST /bookings``` : Create a new booking by submitting booking details in JSON format.
 
-### Example :
+### Examples :
+#### GET the dishes
 Request : 
 ```
-curl http://127.0.0.1:8080/songs/song1
+curl http://127.0.0.1:8080/dishes
 ```
 Response : 
 ```json
-{
-    "name": "song1",
-    "artist": "Nils"
-}
+[
+    {
+        "title": "Margherita Pizza",
+        "description": "Tomato, Mozzarella, Basil, Olive Oil",
+        "allergens": "Cheese, Olive oil",
+        "image": "dishImages.pizza_margherita",
+        "category": "pizza",
+        "price": 10
+    },
+    {
+        "title": "Four Cheese Pizza",
+        "description": "Tomato, Mozzarella, Gorgonzola, Parmesan, Fontina, Provolone",
+        "allergens": "Cheese",
+        "image": "dishImages.pizza_four_cheese",
+        "category": "pizza",
+        "price": 13
+    }
+]
+```
+
+#### POST a booking
+Request : 
+```
+curl -X POST http://127.0.0.1:8080/bookings \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Alice Martin",
+  "phonePrefix": "+33",
+  "phoneNumber": "987654321",
+  "date": "14/12/2024",
+  "time": "19h30"
+}'
+```
+Response from API (data file has been updated): 
+```
+Booking successfully added
 ```
 
 ## Running the app 
--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,29 @@ use std::sync::Mutex;
 
 //------------------------------------------- data structure declaration --------------------------------------------------------
 #[derive(Serialize, Deserialize)]
-struct Song{
-    name:String,
-    artist:String
+struct MenuItem {
+    title: String,
+    description: String,
+    allergens: String,
+    image: String,
+    category: String,
+    price: i32,
 }
 
 #[derive(Serialize, Deserialize)]
-struct MenuItem{
-    dish:String,
-    price:i32
-}
-
-#[derive(Serialize,Deserialize)]
-struct Restaurant{
-    name:String,
-    address:String,
-    menu: Vec<MenuItem>
+struct Booking {
+    name: String,
+    phonePrefix: String,
+    phoneNumber: String,
+    date: String,
+    time: String,
 }
 
 //datafile
 #[derive(Serialize, Deserialize)]
 struct DataFile{
-    restaurants:Vec<Restaurant>,
-    songs:Vec<Song>,
+    dishes: Vec<MenuItem>,
+    bookings: Vec<Booking>,
 }
 
 struct AppState {
@@ -50,48 +50,30 @@ fn write_data(file_path: &str, data_file:&DataFile) {
 
 //-------------------------------------------------- endpoints ------------------------------------------------------------------
 
-#[get("/restaurants")]
-async fn get_restaurants(data: web::Data<AppState>) -> impl Responder {
+// GET all dishes
+#[get("/dishes")]
+async fn get_dishes(data: web::Data<AppState>) -> impl Responder {
     let data_file = read_data(&data.data_file.lock().unwrap());
-    
-    println!("Received GET request: /restaurants"); //for basic log
 
-    HttpResponse::Ok().json(data_file.restaurants)
+    println!("Received GET request: /dishes"); //for basic log
+
+    HttpResponse::Ok().json(data_file.dishes)
 }
 
-#[get("/restaurants/{name}/menu")]
-async fn get_restaurant_menu(data: web::Data<AppState>, name: web::Path<String>) -> impl Responder {
-    let data_file = read_data(&data.data_file.lock().unwrap());
+// POST a new booking
+#[post("/bookings")]
+async fn post_booking(data: web::Data<AppState>, new_booking: web::Json<Booking>) -> impl Responder {
+    let mut data_file = read_data(&data.data_file.lock().unwrap());
 
-    println!("Received GET request: /restaurants/{}/menu", name); //for basic log
+    println!("Received POST request: /bookings"); //for basic log
 
-    if let Some(restaurant) = data_file.restaurants.iter().find(|r| r.name == *name) {
-        HttpResponse::Ok().json(&restaurant.menu)
-    } else {
-        HttpResponse::NotFound().body("Restaurant not found")
-    }
-}
+    //add the new booking to the existing list
+    data_file.bookings.push(new_booking.into_inner());
 
-#[get("/songs/{name}")]
-async fn get_song_info(data: web::Data<AppState>, name: web::Path<String>) -> impl Responder {
-    let data_file = read_data(&data.data_file.lock().unwrap());
+    //save the updated data back to the JSON file
+    write_data(&data.data_file.lock().unwrap(), &data_file);
 
-    println!("Received GET request: /songs/{}", name); //for basic log
-
-    if let Some(song) = data_file.songs.iter().find(|r| r.name == *name) {
-        HttpResponse::Ok().json(&song)
-    } else {
-        HttpResponse::NotFound().body("Song not found")
-    }
-}
-
-#[get("/songs")]
-async fn get_songs(data: web::Data<AppState>) -> impl Responder {
-    let data_file = read_data(&data.data_file.lock().unwrap());
-
-    println!("Received GET request: /songs"); //for basic log
-
-    HttpResponse::Ok().json(data_file.songs)
+    HttpResponse::Ok().body("Booking successfully added")
 }
 
 //-------------------------------------------------------- main -----------------------------------------------------------------
@@ -99,7 +81,7 @@ async fn get_songs(data: web::Data<AppState>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //filepath of the JSON "database"
-    let data_file = "data/data.json".to_string();
+    let data_file = "data/data2.json".to_string();
 
     println!("Starting server on http://127.0.0.1:8080"); //for basic log
 
@@ -109,10 +91,8 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(AppState {
                 data_file: Mutex::new(data_file.clone()),
             }))
-            .service(get_restaurants)
-            .service(get_restaurant_menu)
-            .service(get_songs)
-            .service(get_song_info)
+            .service(get_dishes)    //GET /dishes
+            .service(post_booking)  //POST /bookings
     })
     .bind("127.0.0.1:8080")?
     .run()
